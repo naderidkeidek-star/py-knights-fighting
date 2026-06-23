0c3d13e0d35a1294ba88d548c4ad9e5c763bc8f5
Nader Idkeidek <nader.idkeidek@gmail.com>
2026-06-23 13:39:49 +0300

    Delete app/models/models directory

--- app/models/armour.py
+++
@@ -1,6 +0,0 @@
-# app/models/armour.py
-
-class Armour:
-    def __init__(self, part: str, protection: int) -> None:
-        self.part = part
-        self.protection = protection
--- app/models/weapon.py
+++
@@ -1,6 +0,0 @@
-# app/models/weapon.py
-
-class Weapon:
-    def __init__(self, name: str, power: int) -> None:
-        self.name = name
-        self.power = power
